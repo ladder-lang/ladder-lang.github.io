75e321f2750dfa4444d3baab65cfc3c325a00b64
ladder-lang <mail@ladder-lang.org>
2025-02-05 20:24:26 -0500

    theming

--- editor/ContactNode.jsx
+++ editor/ContactNode.jsx
@@ -9,6 +9,9 @@ function TextUpdaterNode({ data, isConnectable }) {
   useEffect(() => {
     const ctx = canvas.current.getContext("2d");
     const delta = 10;
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.globalAlpha = 1;
     ctx.strokeStyle = "white";
     ctx.moveTo(0, 25);
     ctx.lineTo(10+delta, 25);
@@ -19,6 +22,17 @@ function TextUpdaterNode({ data, isConnectable }) {
     ctx.moveTo(30+delta, 25);
     ctx.lineTo(40+2*delta, 25);
     ctx.stroke();
+    ctx.closePath();
+        ctx.beginPath();
+        ctx.globalAlpha = 0.6;
+        ctx.strokeStyle = 'rgb(57, 255, 20)';
+        ctx.lineWidth = 6;
+        ctx.moveTo(0 + delta*0.8, 25);
+        ctx.lineTo(9 + delta, 25);
+        ctx.moveTo(31 + delta, 25);
+        ctx.lineTo(40 + delta*1.2, 25);
+        ctx.stroke();
+        ctx.closePath();
   }, []);
 
   const onChange = useCallback((evt) => {
